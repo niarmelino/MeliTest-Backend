30a21adb0761d832e1dce5dbc04c8a66a8568ff9
Nicolas Armelino <niarmelino@gmail.com>
2021-02-10 06:18:31 -0300

    Llamada a la lista de de productos

---
+++ README.md
@@ -0,0 +1,11 @@
+Para inicializar el proyecto, primero ejecutar
+
+´´´
+npm i
+´´´
+
+Luego, para inicarlo
+
+´´´
+npm run start:dev
+´´´
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const request = require('request');
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.json({
@@ -12,7 +14,58 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/items", (req, res) => {
-	
+	let query = req.query.q;
+
+	try {
+		request({
+			uri: "https://api.mercadolibre.com/sites/MLA/search",
+			qs: {
+				limit: process.env.CANT_PRODUCTOS,
+				q: query
+			}
+		},
+			function (error, response, body) {
+				if (!error && response.statusCode === 200) {
+					body = JSON.parse(body);
+
+					let Articulos = [];
+
+					for (let item of body.results) {
+						Articulos.push({
+							id: item.id,
+							title: item.title,
+							price: {
+								currency: item.currency_id,
+								amount: item.price,
+								decimals: 0
+							},
+							picture: item.thumbnail,
+							condition: item.condition,
+							free_shipping: item.shipping.free_shipping
+						});
+					}
+
+					res.json({
+						author: {
+							name: "Nicolás",
+							lastname: "Armelino"
+						},
+						items: Articulos
+					});
+				} else {
+					res.json(error);
+				}
+			}
+		);
+	}
+	catch (err) {
+		console.log(err);
+
+		res.status(500).json({
+			ok: false,
+			msg: "Error de servidor."
+		})
+	}
 });
 
 app.listen(process.env.PUERTO, () => {
